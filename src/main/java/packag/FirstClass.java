daa3dec5b0c70fd7305aaa0b42445853f1641056
KartikPatel1997 <128299611+KartikPatel1997@users.noreply.github.com>
2025-12-25 16:48:24 +0530

    This is my first commit and pushing

---
+++ src/main/java/packag/FirstClass.java
@@ -0,0 +1,9 @@
+package packag;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+     System.out.println("This is my first commit into tetsing class called - FirstClass.");
+	}
+
+}
