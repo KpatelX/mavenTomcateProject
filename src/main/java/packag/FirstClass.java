283c42fa5e53cefb3af3be7297b969f0bc713b88
KartikPatel1997 <128299611+KartikPatel1997@users.noreply.github.com>
2025-12-26 17:17:57 +0530

    make space

--- src/main/java/packag/FirstClass.java
+++ src/main/java/packag/FirstClass.java
@@ -5,6 +5,6 @@ public class FirstClass {
 	public static void main(String[] args) {
      System.out.println("This is my first commit into tetsing class called - FirstClass.");
      System.out.println(" again chnages This is second commit");
-     System.out.println(" This is all set commit");
+          System.out.println(" This is all set commit");
 	}
 }
